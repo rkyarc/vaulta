521870b0cdf7272d37c36d2c6ecc7659e945d473
rkyarc <arickyh1@gmail.com>
2026-08-14 06:35:00 +0700

    feat: membuat rancangan schema database untuk users, categories, dan transactions

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,1 +1,29 @@
-export {};
+import { pgTable, serial, varchar, integer, timestamp } from "drizzle-orm/pg-core";
+
+// 1. Table User
+export const users = pgTable("users", {
+    id: serial("id").primaryKey(),
+    name: varchar("name", { length: 255 }).notNull(),
+    email: varchar("email", { length: 255}).notNull().unique(),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+});
+
+// 2. Table Categories
+export const categories = pgTable("categories", {
+    id: serial("id").primaryKey(),
+    name: varchar("name", { length: 255 }).notNull(),
+    type: varchar("type", { length: 50 }).notNull(),
+    userId: integer("user_id").references(() => users.id).notNull(),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+});
+
+// 3. Table Transactions
+export const transactions = pgTable("transactions", {
+    id: serial("id").primaryKey(),
+    amount: integer("amount").notNull(),
+    description: varchar("description", { length: 255 }).notNull(),
+    date: timestamp("date").notNull(),
+    userId: integer("user_id").references(() => users.id).notNull(),
+    categoryId: integer("category_id").references(() => categories.id).notNull(),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+});
